3924dcdb31fa07b6a29a333a329945c81832a84f
MatchuPitchu <michael.flohr@posteo.de>
2023-08-23 13:57:07 +0200

    feat: progress with course

---
+++ src/02-unions-and-indexing/10-as-const.ts
@@ -0,0 +1,32 @@
+import { Equal, Expect } from '../helpers/type-utils';
+
+/**
+ * Some docs that might help!
+ * https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-4.html#const-assertions
+ */
+export const obj = {
+  GROUP: 'group',
+  ANNOUNCEMENT: 'announcement',
+  ONE_ON_ONE: '1on1',
+  SELF_DIRECTED: 'selfDirected',
+  PLANNED_ONE_ON_ONE: 'planned1on1',
+  PLANNED_SELF_DIRECTED: 'plannedSelfDirected',
+} as const;
+
+type Obj = typeof obj;
+
+export type GroupProgram = Obj['GROUP'];
+export type AnnouncementProgram = Obj['ANNOUNCEMENT'];
+export type OneOnOneProgram = Obj['ONE_ON_ONE'];
+export type SelfDirectedProgram = Obj['SELF_DIRECTED'];
+export type PlannedOneOnOneProgram = Obj['PLANNED_ONE_ON_ONE'];
+export type PlannedSelfDirectedProgram = Obj['PLANNED_SELF_DIRECTED'];
+
+type tests = [
+  Expect<Equal<GroupProgram, 'group'>>,
+  Expect<Equal<AnnouncementProgram, 'announcement'>>,
+  Expect<Equal<OneOnOneProgram, '1on1'>>,
+  Expect<Equal<SelfDirectedProgram, 'selfDirected'>>,
+  Expect<Equal<PlannedOneOnOneProgram, 'planned1on1'>>,
+  Expect<Equal<PlannedSelfDirectedProgram, 'plannedSelfDirected'>>,
+];
---
+++ src/02-unions-and-indexing/11-indexed-access-with-unions.ts
@@ -0,0 +1,21 @@
+import { Equal, Expect } from '../helpers/type-utils';
+
+export const obj = {
+  GROUP: 'group',
+  ANNOUNCEMENT: 'announcement',
+  ONE_ON_ONE: '1on1',
+  SELF_DIRECTED: 'selfDirected',
+  PLANNED_ONE_ON_ONE: 'planned1on1',
+  PLANNED_SELF_DIRECTED: 'plannedSelfDirected',
+} as const;
+
+type Obj = typeof obj;
+
+// passing a union into an indexed access type like this will return another union
+export type IndividualProgram1 = Obj['ONE_ON_ONE' | 'SELF_DIRECTED' | 'PLANNED_ONE_ON_ONE' | 'PLANNED_SELF_DIRECTED'];
+
+// create a union by excluding the members that you don't want
+export type IndividualProgram2 = Obj[Exclude<keyof Obj, 'GROUP' | 'ANNOUNCEMENT'>];
+
+type tests1 = [Expect<Equal<IndividualProgram1, '1on1' | 'selfDirected' | 'planned1on1' | 'plannedSelfDirected'>>];
+type tests2 = [Expect<Equal<IndividualProgram2, '1on1' | 'selfDirected' | 'planned1on1' | 'plannedSelfDirected'>>];
