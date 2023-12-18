27c2ec5f3fa6cd74980523918cecf71769204808
MatchuPitchu <michael.flohr@posteo.de>
2023-12-18 20:00:31 +0100

    feat: add course progress

--- src/advanced-patterns/04-classes/19.4-alex-trpc-builder-pattern.explainer.ts
+++ src/advanced-patterns/04-classes/19.4-alex-trpc-builder-pattern.explainer.ts
@@ -1,3 +1,5 @@
 // Alex from tRPC explains the builder pattern
+// appRouter, source from the video: https://github.com/trpc/trpc/blob/main/examples/minimal/server/index.ts#L11
+// TRPC Documentation: https://trpc.io/
 
 export {};
